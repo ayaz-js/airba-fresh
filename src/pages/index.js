454b3ecd797c5ecd3bfe9b295a24909d87cebed3
Serik Ayazbayev <jetheez@yandex.ru>
2022-11-07 15:01:14 +0600

    feat: добавить сообщение в консоле

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
 import './index.scss';
+
+const emoji = ['😸', '🏄', '🚀', '🔥', '🎉', '😄'];
+
+const randomEmoji = () => {
+    const random = Math.floor(Math.random() * emoji.length);
+
+    return emoji[random];
+};
+
+console.log(
+`%chello! ${randomEmoji()}. developed by ayazbayev serik`,
+'color: #fff; background-color: #77C043; padding: 6px 8px; font-family: "Segoe UI"; font-size: 14px;',
+);

dd639d485cd3175cfb63fcf668603b7b2abc674e
Serik Ayazbayev <jetheez@yandex.ru>
2022-11-10 17:22:54 +0600

    Updates

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var o=["🙃","😸","🏄","🚀","🔥","😄","🖖"];console.log("%chello! ".concat(o[Math.floor(Math.random()*o.length)],". developed by ayazbayev serik"),'font-family: "Segoe UI"; font-size: 14px; color: #fff; background-color: #77C043; padding: 6px 8px;')})();

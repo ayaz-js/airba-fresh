ea6a28315c7400f581afde251008850297ec0ce0
Serik Ayazbayev <jetheez@yandex.ru>
2022-11-10 17:13:31 +0600

    feat: добавить ссылку на WA

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import './index.scss';
 
-const emoji = ['😸', '🏄', '🚀', '🔥', '🎉', '😄'];
+const emoji = ['🙃', '😸', '🏄', '🚀', '🔥', '😄', '🖖'];
+
+const consoleStyles = {
+    styles: 'font-family: "Segoe UI"; font-size: 14px; color: #fff; background-color: #77C043; padding: 6px 8px;',
+};
 
 const randomEmoji = () => {
     const random = Math.floor(Math.random() * emoji.length);
@@ -8,7 +12,4 @@ const randomEmoji = () => {
     return emoji[random];
 };
 
-console.log(
-`%chello! ${randomEmoji()}. developed by ayazbayev serik`,
-'color: #fff; background-color: #77C043; padding: 6px 8px; font-family: "Segoe UI"; font-size: 14px;',
-);
+console.log(`%chello! ${randomEmoji()}. developed by ayazbayev serik`, consoleStyles.styles);
